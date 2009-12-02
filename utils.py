4d01b9e3d2109b20ef447007770464f8a616d984
Julien Bouquillon <julien@bouquillon.com>
2009-12-02 16:15:08 +0100

    set_cookie in utils

--- utils.py
+++ utils.py
@@ -1,9 +1,20 @@
+import datetime
 
 # extjs special encoder
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 
 
-
+def set_cookie(response, key, value, days_expire = 7):
+    if days_expire is None:
+        max_age = 365*24*60*60  #one year
+    else:
+        max_age = days_expire*24*60*60 
+        
+    expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+    response.set_cookie(key, value, max_age=max_age, expires=expires)
+    return response
+    
+ 
 def DateFormatConverter(to_extjs = None, to_python = None):
     """ convert date formats between ext and python """
     f = {}
